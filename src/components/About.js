5c11b23961a6c2c1c91122042cc798a8f84df3ff
Cameron Jones <iamcrjones@gmail.com>
2022-06-01 19:02:50 +1000

    styling and added some about me text

--- src/components/About.js
+++ src/components/About.js
@@ -1,44 +1,14 @@
 const About = () => {
     return(
         <div id="aboutContainer">
-            <h2>About Section</h2>
-            <p>about section</p>
-            <p>about section</p>
-            <p>about section</p>
-            <p>about section</p>
-            <p>about section</p>
-            <p>about section</p>
-            <p>about section</p>
-            <p>about section</p>
-            <p>about section</p>
-            <p>about section</p>
-            <p>about section</p>
-            <p>about section</p>
-            <p>about section</p>
-            <p>about section</p>
-            <p>about section</p>
-            <p>about section</p>
-            <p>about section</p>
-            <p>about section</p>
-            <p>about section</p>
-            <p>about section</p>
-            <p>about section</p>
-            <p>about section</p>
-            <p>about section</p>
-            <p>about section</p>
-            <p>about section</p>
-            <p>about section</p>
-            <p>about section</p>
-            <p>about section</p>
-            <p>about section</p>
-            <p>about section</p>
-            <p>about section</p>
-            <p>about section</p>
-            <p>about section</p>
-            <p>about section</p>
-            <p>about section</p>
-            <p>about section</p>
-
+                <div className="banner">
+                    <h2>About Me</h2>
+                </div>
+                <div className='aboutContent'>
+                    <p> After working in the retail industry for the past few years, I decided to take the plunge into learning full stack web development through the flex track course provided by Coder Academy.</p>
+                    <p>Although I'm studying full stack development, I am wanting to mainly focus on working in the front end as I enjoy it a lot!</p>
+                    <p>Check down below for projects I have done / am working on to get an idea of what I can do.</p>
+                </div>
         </div>
     )
 }
--- src/styled/Global.style.js
+++ src/styled/Global.style.js
@@ -6,12 +6,13 @@ const colors = {
     color3: '#FF7477',
     color4: '#B5D6D6',
     color5: '#9CF6F6',
-    color6: '#E5ECE9'
+    color6: '#EFF1F3',
+    color7: '#223843'
 }
 
 const GlobalStyles = createGlobalStyle`
     body {
-        background-image: linear-gradient(to bottom right, ${colors.color1}, ${colors.color4});
+        background-color: ${colors.color6};
         // color: ${colors.color3}
         font-family: 'IBM Plex Sans', sans-serif;
     }
@@ -19,7 +20,7 @@ const GlobalStyles = createGlobalStyle`
         display: flex;
         align-items: flex-start;
         border: 1px solid black;
-        background-color: ${colors.color4};
+        background-color: ${colors.color2};
         color: ${colors.color3};
     }
     .navBar ul {
@@ -31,9 +32,21 @@ const GlobalStyles = createGlobalStyle`
     .navBar li {
         display: flex;
         list-style-type: none;
-        border: 2px solid green;
+        border: 2px solid ${colors.color1};
         padding: 5px;
         margin: 5px;
+        background-color: ${colors.color3};
+        color: black;
+    }
+    .banner {
+        display: flex;
+        justify-content: center;
+        border: 2px solid red;
+        background-color: ${colors.color5};
+        padding: 0.5em 0;
+    }
+    .aboutContent {
+        padding: 0 5%;
     }
 `
 export default GlobalStyles;
