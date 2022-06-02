75876607aa2b425a587e1994626f88cb5d0d47f8
Cameron Jones <iamcrjones@gmail.com>
2022-06-02 12:39:52 +1000

    contact banner

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,6 +2,9 @@ const Contact = () => {
     return(
         <>
             <div id="contactContainer">
+                <div className="banner">
+                    <h2>Contact Info</h2>
+                </div>
                 <h4>hello pls gib job</h4>
                 <h4>hello pls gib job</h4>
                 <h4>hello pls gib job</h4>
